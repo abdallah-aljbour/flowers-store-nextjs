86d31bc36a8c8b1deab5f4e027c34410cfcf0532
abdallah <abdallajbour@outlook.com>
2025-12-24 20:48:35 +0300

    fixed env file

--- config/firebase.js
+++ config/firebase.js
@@ -9,7 +9,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log("🔥 Firebase Config:", {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+});
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
